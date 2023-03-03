dfd09614f080872b739638c986e28416a43d4c8b
Marcelo Gonçalves <mgscosta@ice.ufjf.br>
2023-03-03 01:26:46 -0300

    Exercicio 4

---
+++ desafio_target_4/FaturamentoEstadoModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace desafio_target_4
+{
+    internal class FaturamentoEstadoModel
+    {
+        public string estado { get; set; }
+        public double faturamento { get; set; }
+    }
+}
---
+++ desafio_target_4/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Text.Json;
+
+namespace desafio_target_4;
+
+class Program
+{
+
+    public static Dictionary<string, double> calculaPercentualRepresentacao(List<FaturamentoEstadoModel> faturamentoEstadoModels)
+    {
+        double valorTotalMensal = 0;
+        Dictionary<string, double> result = new Dictionary<string, double>();
+
+        faturamentoEstadoModels.ForEach(x => valorTotalMensal += x.faturamento);
+
+        Console.WriteLine("Valor Total Mensal: {0}", valorTotalMensal);
+
+        faturamentoEstadoModels.ForEach(x => result.Add(x.estado, x.faturamento * 100 / valorTotalMensal));
+
+        return result;
+    }
+
+    static void Main(string[] args)
+    {
+        List<FaturamentoEstadoModel> faturamentoEstadoModels = new List<FaturamentoEstadoModel>();
+        
+        Dictionary<string, double> result;
+
+        using (StreamReader reader = new StreamReader("data\\faturamento.json"))
+        {
+            string json = reader.ReadToEnd();
+            faturamentoEstadoModels = JsonSerializer.Deserialize<List<FaturamentoEstadoModel>>(json);
+        }
+
+        result = calculaPercentualRepresentacao(faturamentoEstadoModels);
+
+        foreach(var item in result)
+        {
+            Console.WriteLine("Estado: {0}, Porcentagem: {1} %", item.Key, Math.Round(item.Value, 2));
+        }
+    }
+}

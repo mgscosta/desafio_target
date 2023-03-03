d5be743b244edf36c30612eda52fc2d399211693
Marcelo Gonçalves <mgscosta@ice.ufjf.br>
2023-03-03 01:43:38 -0300

    Exercicio 5

---
+++ desafio_target_5/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace desafio_target_5;
+class Program
+{
+    public static string inverteString(string str)
+    {
+        char [] arr = str.ToArray();
+
+        List<char> aux = new List<char>();
+        
+        for(int i = str.Length - 1; i >= 0; i--)
+        {
+            aux.Add(str[i]);
+        }
+
+        return String.Join("", aux);
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine(inverteString("hello world"));
+    }
+}
